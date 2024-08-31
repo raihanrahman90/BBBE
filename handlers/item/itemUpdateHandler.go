d2c6d9de822cd1c1faa450ff72b30d095361aa04
Muhammad Raihan Rahman <raihanr090@gmail.com>
2024-09-01 00:59:15 +0800

    update validation image path

--- handlers/item/itemUpdateHandler.go
+++ handlers/item/itemUpdateHandler.go
@@ -2,6 +2,7 @@ package item
 
 import (
 	"net/http"
+	"strings"
 
 	"bbbe/config"
 	"bbbe/models"
@@ -16,6 +17,7 @@ func UpdateItem(c *gin.Context) {
 		Name        string `json:"name"`
 		Price       int    `json:"price"`
 		Description string `json:"description"`
+		Image		string `json:"image"`
 	}
 	var item models.Item
 	id := c.Param("id")
@@ -33,6 +35,15 @@ func UpdateItem(c *gin.Context) {
 	item.Name = requestData.Name
 	item.Price = requestData.Price
 	item.Description = requestData.Description
+
+	if len(strings.TrimSpace(requestData.Image)) >0 {
+		imagePath, err := utils.SaveBase64Image(requestData.Image)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, utils.FailedResponse(err.Error()))
+			return
+		}
+		item.Image = imagePath
+	}
 
 	if err := config.DB.Save(&item).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, utils.FailedResponse("Failed to save changes"))
